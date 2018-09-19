f4b7a9749f2564926b018ceb710cff19ef152cc9
AlzateEA <andresalzatee@hotmail.com>
2018-09-19 17:31:24 -0500

    Galería final
    -Se finalizó el funcionamiento de la galería

--- js/main.js
+++ js/main.js
@@ -9,5 +9,28 @@ $(document).ready(function(){
             $(".AAE-logo-menu img").removeClass("AAE-header-logo-add");
         }
     }, false);
+    //Galeria inicio
+    $('.img-gallery').magnificPopup({
+        type: 'image',
+        closeBtnInside: false,
+        closeOnContentClick: false,
+    
+    callbacks: {
+      open: function() {
+        var self = this;
+        self.wrap.on('click.pinhandler', 'img', function() {
+          self.wrap.toggleClass('mfp-force-scrollbars');
+        });
+      },
+      beforeClose: function() {
+            this.wrap.off('click.pinhandler');
+        this.wrap.removeClass('mfp-force-scrollbars');
+      }
+    },
+     
+    image: {
+            verticalFit: false
+        }
+    });
+    //Final galeria
 });
-
