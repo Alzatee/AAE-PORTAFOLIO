4182d872d2a307c7ad3a3ad774250dadb215012b
AlzateEA (AAE) <alzateEA@hotmail.com>
2021-05-26 03:07:28 -0500

    Actualización de mi antiguo proyecto de portafolio
    - Le realicé una pequeña modificación a mi antiguo proyecto de practicante de un portafolio,
    me acordé de este y del valor que tiene para mi presentación así que de por mientras le di unos ajustes y lo actualicé
    a algo más acorde con mis nuevos aprendizajes.
    Recordatorio: cuando tenga tiempo quiero reflejar este proyecto a Angular con más enfoque y aplicando nuevos conocimientos, mi portafolio definitivo.

---
+++ js/fullpage.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  var size = screen.width
+  if (size >= '1024') { //SOLO FUNCIONARÁ EL FULL PAGE EN DISPOSITIVOS GRANDES, COMPUTADORAS
+    $('#AAE-fullpage').fullpage({
+      anchors: ['Inicio', 'Sobre_mi', 'Información', 'Servicios', 'Proyectos', 'Pie'],//Nombres de las secciones en la url
+      navigation: true, //barra navegación
+      navigationPosition: 'right',//Posición barra de navegación
+      navigationTooltips: ['Inicio', 'Sobre mi', 'Información', 'Servicios', 'Proyectos', 'Pie'],//Nombres barra de navegación
+      scrollBar: true,
+      scrollingSpeed: 1200,
+
+      afterLoad: function(anchorLink, index) {//ESCONDER HAST Y URL
+        history.pushState(null, null, "index.html");
+      },
+      //Movimiento de la imagen de fondo al hacer scroll
+      afterRender: function (index, nextIndex, direction) {
+        var movimiento_scroll_img = $('.AAE-background-parallax');
+        movimiento_scroll_img.paroller({
+          factor: 0.5,
+          type: 'background',
+          direction: 'vertical',
+        });
+      },
+      afterResize: function() {
+        var movimiento_scroll_img = $('.AAE-background-parallax');
+        movimiento_scroll_img.paroller({
+          factor: 0.5,
+          type: 'background',
+          direction: 'vertical'
+        });
+      }
+    });
+  }
+});
+
+
+//Para conectar el menú nav hay que agregarle a los <a> en el href la id de la sección ej: <a href="#Inicio"
+//Las ids están nombradas en este archivo en Tooltips
+
+//Para poner un section más pequeño o con tamaño que espesifique, agregar al section la clase fp-auto-height luego de la clase section, como está el footer en el html
+
+
+
+
+  
