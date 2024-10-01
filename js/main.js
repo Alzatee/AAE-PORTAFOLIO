517d254e0840224cd4e28ffe20f9a1ed72c4d02d
Aalzatee <alzateEA@hotmail.com>
2024-10-01 17:16:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -129,29 +129,6 @@ $(document).ready(function(){
     }, false);
     /* --- ANIMATION NAVBAR FIN --- */
 
-    /* --- GALERÍA DE PROYECTOS INICIO --- */
-    $('.img-gallery').magnificPopup({
-        type: 'image',
-        closeBtnInside: false,
-        closeOnContentClick: false,
-    callbacks: {
-      open: function() {
-        var self = this;
-        self.wrap.on('click.pinhandler', 'img', function() {
-          self.wrap.toggleClass('mfp-force-scrollbars');
-        });
-      },
-      beforeClose: function() {
-            this.wrap.off('click.pinhandler');
-        this.wrap.removeClass('mfp-force-scrollbars');
-      }
-    },
-    image: {
-            verticalFit: false
-        }
-    });
-    /* --- GALERÍA DE PROYECTOS FIN --- */
-
     //Ancla en dispositivos móviles
     $('.AAE-ancle-up').click(function(anclaMovile){				
 		anclaMovile.preventDefault();		//evitar el eventos del enlace normal
